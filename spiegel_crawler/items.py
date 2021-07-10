e066863011b60388e045791859f4d0253cd5f32b
Logician724 <Logician724@gmail.com>
2021-07-10 17:31:07 +0200

    feat: init spiegel crawler spider

--- spiegel_crawler/items.py
+++ spiegel_crawler/items.py
@@ -5,10 +5,16 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
-
-class SpiegelCrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ArticleItem(Item):
+    id = Field()
+    title = Field()
+    subtitle = Field()
+    abstract = Field()
+    downloaded_at = Field()
+    def format(self):
+        self['id'] =  self.get('id').strip().strip('"')
+        self['title'] = self.get('title').strip().strip('"')
+        self['subtitle'] = self.get('subtitle').strip().strip('"')
+        self['abstract'] = self.get('abstract').strip().strip('"')
---
+++ spiegel_crawler/spiders/spiegel_spider.py
@@ -0,0 +1,37 @@
+import scrapy
+from spiegel_crawler.items import ArticleItem
+import re
+import logging
+import datetime
+class SpiegelCrawler(scrapy.Spider):
+    name = 'spiegel'
+    start_urls = [
+        'https://www.spiegel.de/international/'
+    ]
+
+    def parse(self, response):
+        articles_selectors = response.xpath('//article')
+        for curr_selector in articles_selectors:
+            curr_article = ArticleItem(
+                id = SpiegelCrawler.gen_id(curr_selector),
+                title = curr_selector.xpath('.//header//a[@title]/@title').get(),
+                subtitle = curr_selector.xpath(
+                    './/header//a[@title]//span').css('.text-primary-base').xpath('./text()').get(),
+                abstract = curr_selector.xpath(
+                    '(.//section//a[@title]/span)[1]//text()').get(),
+                downloaded_at = datetime.datetime.now()
+            )
+            yield curr_article
+        next_page = re.search(
+            "'(.*)'", response.xpath('//div[@data-area="pagination-bar"]').xpath('.//span[@title="Ältere Artikel"]/@onclick').get()).group().replace("'","")
+        if next_page is not None:
+            logging.debug(next_page)
+            yield response.follow(next_page, self.parse)
+
+    @staticmethod
+    def gen_id(article_selector):
+        id = article_selector.xpath('@data-sara-article-id').get()
+        # There seems to be a bug in spiegel where some articles have a mistyped id attribute
+        if(id is None):
+            id = article_selector.xpath('@ata-sara-article-id').get()
+        return id
